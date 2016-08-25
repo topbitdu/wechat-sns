1a5b9bd5ce049c3fa61abb7bfe82d3f313ce02d4
Topbit Du <topbit.du@gmail.com>
2016-08-25 10:28:37 +0800

    1, Improve the Authorize Link class for the argument validation.

--- lib/wechat/sns/authorize_link.rb
+++ lib/wechat/sns/authorize_link.rb
@@ -7,6 +7,9 @@ class Wechat::SNS::AuthorizeLink
   # 第一步：用户同意授权，获取code
   # http://mp.weixin.qq.com/wiki/9/01f711493b5a02f24b04365ac5d8fd95.html#.E7.AC.AC.E4.B8.80.E6.AD.A5.EF.BC.9A.E7.94.A8.E6.88.B7.E5.90.8C.E6.84.8F.E6.8E.88.E6.9D.83.EF.BC.8C.E8.8E.B7.E5.8F.96code
   def self.create(wechat_app_id, redirect_link, response_type: Wechat::SNS::AuthorizeLink::RESPONSE_TYPE_CODE, scope: Wechat::SNS::AuthorizeLink::SCOPE_FULL, state: nil)
+
+    raise ArgumentError.new('The wechat_app_id argument is required.') if wechat_app_id.blank?
+
     "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{wechat_app_id}&redirect_uri=#{CGI::escape redirect_link}&response_type=#{response_type}&scope=#{scope}&state=#{state}#wechat_redirect"
   end
 
