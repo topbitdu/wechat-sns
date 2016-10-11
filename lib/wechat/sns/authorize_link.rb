060630966265676acd5af846912b5bd059e83ca9
Topbit Du <topbit.du@gmail.com>
2016-10-11 10:09:47 +0800

    1, Improve the Access Token wrapper class for the argument validation.

--- lib/wechat/sns/authorize_link.rb
+++ lib/wechat/sns/authorize_link.rb
@@ -11,6 +11,7 @@ class Wechat::SNS::AuthorizeLink
   def self.create(wechat_app_id, redirect_link, response_type: Wechat::SNS::AuthorizeLink::RESPONSE_TYPE_CODE, scope: Wechat::SNS::AuthorizeLink::SCOPE_FULL, state: nil)
 
     assert_present! :wechat_app_id, wechat_app_id
+    assert_present! :redirect_link, redirect_link
     #raise ArgumentError.new('The wechat_app_id argument is required.') if wechat_app_id.blank?
 
     "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{wechat_app_id}&redirect_uri=#{CGI::escape redirect_link}&response_type=#{response_type}&scope=#{scope}&state=#{state}#wechat_redirect"
